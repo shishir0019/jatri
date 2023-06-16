80d59268f9e0ccab140a59753faeb1ac266a6918
blackCat <shishir0019@gmail.com>
2023-06-17 02:11:18 +0600

    'finish'

--- src/components/Expandable.vue
+++ src/components/Expandable.vue
@@ -10,9 +10,13 @@ const props = defineProps<Props>()
 
 <template>
     <div class="border rounded-md p-3">
-        <h3 class="text-xl">Title: {{ row.title }}</h3>
-        <div>Price: {{ row.price }}</div>
-        <div>Rating: {{ row.rating }}</div>
-        <div>Description: {{ row.description }}</div>
+        <h1 class="text-center text-2xl my-4">View Details</h1>
+        <hr>
+        <div class="my-4">
+            <h3 class="text-xl">Title: {{ row.title }}</h3>
+            <div>Price: {{ row.price }}</div>
+            <div>Rating: {{ row.rating }}</div>
+            <div>Description: {{ row.description }}</div>
+        </div>
     </div>
 </template>
